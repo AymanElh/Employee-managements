2be3c30755ffdd2cc2c42a9b773ce1f6d549339b
Ayman Elh <elhamioui1964@gmail.com>
2025-05-13 11:29:35 +0100

    create models and migrations

--- database/migrations/2025_05_13_101756_create_employees_table.php
+++ database/migrations/2025_05_13_101756_create_employees_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('country_id')->constrained()->onDelete('set null');
+            $table->foreignId('state_id')->constrained()->onDelete('set null');
+            $table->foreignId('city_id')->constrained()->onDelete('set null');
+            $table->foreignId('department_id')->constrained()->onDelete('set null');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('address');
+            $table->char('zip_code');
+            $table->date('date_of_birth');
+            $table->date('date_hired');
             $table->timestamps();
         });
     }
